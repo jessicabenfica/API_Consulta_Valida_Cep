23db789ee3c10e3a0c24a47753cdbb658b216054
Jessica Benfica <jessicabenfica28@gmail.com>
2019-12-13 10:54:10 -0300

    Validação e Consulta API Cep

---
+++ Models/CorreiosResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Correios.Models
+{
+    public class CorreiosResponse
+    {
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string UF { get; set; }
+        public string Unidade { get; set; }
+        public string Ibge { get; set; }
+        public string Gia { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using RestSharp;
+using Xunit;
+using FluentAssertions;
+using Correios.Models;
+using Newtonsoft.Json;
+using NUnit.Framework.Internal;
+
+namespace Correios
+{
+    public class Program
+    {
+        private IRestResponse GetCEP(object CEP)
+        {
+            var client = new RestClient("https://viacep.com.br/ws/" + CEP + "/json/");
+         // var client = new RestClient("https://viacep.com.br/ws/01001000/json");
+            var RSrequest = new RestRequest(Method.GET) { RequestFormat = DataFormat.Json };
+            return client.Execute(RSrequest);
+        }
+
+       /* [Fact]
+        public void CasoSucesso_1()
+        {
+            var response = GetCEP("01001000");
+            response.StatusCode.Should().Be(200);
+        } */
+
+        [Fact]
+        public void CasoSucesso_2()
+        {
+            var response = GetCEP("01001000");
+            var json = JsonConvert.DeserializeObject<CorreiosResponse>(response.Content); // Deserealizar o conteúdo do response na variável “json”.
+
+            //validação dos campos 
+            json.Cep.Should().Be("01001-000");
+            json.Logradouro.Should().Be("Praça da Sé");
+            json.Complemento.Should().Be("lado ímpar");
+            json.Bairro.Should().Be("Sé");
+            json.Localidade.Should().Be("São Paulo");
+            json.UF.Should().Be("SP");
+            json.Unidade.Should().Be("");
+            json.Ibge.Should().Be("3550308");
+            json.Gia.Should().Be("1004");
+
+            response.StatusCode.Should().Be(200); // code = 200 - Dados enviados com sucesso. 
+           
+            
+       
+        }
+    }
+}
